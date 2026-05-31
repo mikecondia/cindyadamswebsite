ba263ac9fb4b8ccc08819816b31bd4d95ba35c9a
Mike Condia <mikecondia@gmail.com>
2026-05-30 21:35:13 -0700

    set up navbar and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div>
-      <Navbar /> 
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 
